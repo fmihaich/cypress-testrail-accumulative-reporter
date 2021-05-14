abc332d913fd392c795ba9aa3144b652bac1dbe7
Florencia Mihaich <florencia.mihaich@commonbond.co>
2021-05-14 11:40:56 -0300

    Reverting unnecesary loging

--- dist/cypress-testrail-reporter.js
+++ dist/cypress-testrail-reporter.js
@@ -28,12 +28,6 @@ var CypressTestRailReporter = /** @class */ (function (_super) {
         _this.validate(reporterOptions, 'password');
         _this.validate(reporterOptions, 'projectId');
         _this.validate(reporterOptions, 'suiteId');
-        runner.on('suite', function () {
-            console.log('**** SUITE STARTS ****');
-        });
-        runner.on('suite end', function () {
-            console.log('**** SUITE ENDS ****');
-        });
         runner.on('start', function () {
             var executionDateTime = moment().format('MMM Do YYYY, HH:mm (Z)');
             var name = (reporterOptions.runName || 'Automated test run') + " " + executionDateTime;
